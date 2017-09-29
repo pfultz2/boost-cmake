15cd96906050850144d0953349067898672672ca
Paul <pfultz2@yahoo.com>
2017-09-28 19:12:44 -0500

    Optionally exclude python and mpi

--- root.cmake
+++ root.cmake
@@ -12,11 +12,31 @@ endmacro()
 
 include(bcm/share/bcm/cmake/BCMConfig.cmake)
 
+set(EXCLUDE_LIBS)
+
+find_package(PythonLibs)
+if(NOT PythonLibs_FOUND)
+    list(APPEND EXCLUDE_LIBS python)
+endif()
+
+find_package(PythonLibs)
+if(NOT PythonLibs_FOUND)
+    list(APPEND EXCLUDE_LIBS python)
+endif()
+
+find_package(MPI)
+if(NOT MPI_FOUND)
+    list(APPEND EXCLUDE_LIBS mpi)
+endif()
+
 file(GLOB_RECURSE LIBS libs/*CMakeLists.txt)
 foreach(lib ${LIBS})
     file(READ ${lib} CONTENT)
     if("${CONTENT}" MATCHES "project\\(")
         get_filename_component(LIB_DIR ${lib} DIRECTORY)
-        add_subdirectory(${LIB_DIR})
+        get_filename_component(LIB_NAME ${LIB_DIR} NAME)
+        if(NOT "${LIB_NAME}" IN_LIST EXCLUDE_LIBS)
+            add_subdirectory(${LIB_DIR})
+        endif()
     endif()
 endforeach()
